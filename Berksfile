5de7c9c54b94a7f60c5b9e216cdf27326337e832
Marc Bux <buxmarcn@informatik.hu-berlin.de>
2015-11-17 16:07:07 +0100

    restored berksfile, kitchenfile, metadata

--- Berksfile
+++ Berksfile
@@ -2,7 +2,5 @@ source 'https://supermarket.chef.io'
 
 metadata
 
-cookbook 'java'
-cookbook 'git', '<4.2.3'
 cookbook "chef-bioinf-worker", github: "joergen7/chef-bioinf-worker"
 cookbook "chef-cuneiform", github: "joergen7/chef-cuneiform"
--- metadata.rb
+++ metadata.rb
@@ -1,16 +1,11 @@
 name 'variant-call'
-maintainer "Jörgen Brandt"
+maintainer "Joergen Brandt"
 maintainer_email "brandjoe@hu-berlin.de"
 license 'all_rights'
 description 'Installs/Configures variant-call'
 long_description 'Installs/Configures variant-call'
 version '0.1.0'
 
-depends "java"
-depends "git"
-depends "hadoop"
-depends "kagent"
-depends "git"
 depends "chef-bioinf-worker"
 depends "chef-cuneiform"
 
